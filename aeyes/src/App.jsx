19812abab2c9f4cb3f4b24730e32b0350452d6e1
Koh Chee Kiat <66861775+jaga991@users.noreply.github.com>
2023-08-27 12:12:39 +0800

    mvp done

--- aeyes/src/App.jsx
+++ aeyes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 import Dropzone from 'react-dropzone'
 import "./styles.css";
 
@@ -7,10 +7,70 @@ import "./styles.css";
 
 function App() {
   const [fileNames, setFileNames] = useState([]);
-  const handleDrop = acceptedFiles => {
-    setFileNames(acceptedFiles.map(file => file.name));
-    console.log(acceptedFiles);
+  const [extractedHtml, setExtractedHTML] = useState("")
+  const [oldCode, setOldCode] = useState("")
+  const [newCode, setNewCode] = useState("")
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false)
+  const handleDrop = async acceptedFiles => {
+    try {
+      const formData = new FormData();
+      acceptedFiles.forEach(file => {
+        formData.append("files", file); // Use "files" as the form field name
+      });
+      const response = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData,
+      });
+  
+      if (response.ok) {
+        const data = await response.json(); // Parse the JSON response
+        console.log(data)
+      } else {
+        console.error("File upload failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+
+    
   }
+
+  const handleDownload = async () => {
+    try {
+      setIsButtonEnabled(false); // Disable the button while the request is in progress
+
+      const response = await fetch("http://localhost:5000/download", {
+        method: "GET",
+        // You can add headers or other configurations here if needed
+      });
+
+      if (response.ok) {
+        const blob = await response.blob();
+
+        // Create a temporary URL for the blob
+        const url = window.URL.createObjectURL(blob);
+  
+        // Create a temporary anchor element to trigger the download
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "filename.html"; // Set the desired filename and extension
+        document.body.appendChild(a);
+        a.click();
+  
+        // Clean up the temporary URL and anchor element
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+  
+      } else {
+        // Handle the error case, e.g., show an error message
+        console.error("Download failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    } finally {
+      setIsButtonEnabled(true); // Re-enable the button after the request is complete
+    }
+  };
 
   return(
     <Box
@@ -19,8 +79,9 @@ function App() {
       backgroundColor="#151322"  // Example background color
       color="white" // Example text color
       display="flex"
-      justifyContent="center"
-      alignItems="start"
+      flexDir="column"
+      justifyContent="start"
+      alignItems="center"
     >
       <Box
         w="100vw"    // Set width to 100 viewport width
@@ -59,6 +120,27 @@ function App() {
             );
           }}
         </Dropzone>
+      </Box>
+          
+      <Box
+        w="100vw" 
+        backgroundColor="#133333"  // Example background color
+        border="2px solid white"
+        display="flex"
+        flexDir="row"
+        justifyContent="start"
+        alignItems="center"
+      >
+  <Button
+    loadingText='Download'
+    colorScheme='teal'
+    variant='solid'
+    alignSelf='center'
+    justifySelf='center'
+    onClick={handleDownload}
+  >
+    Download
+  </Button>
       </Box>
     </Box>
   )
---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, send_file
+from gradio_client import Client
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/upload": {"origins": "http://127.0.0.1:5173"}})  # Adjust with your frontend URL
+CORS(app, resources={r"/download": {"origins": "http://127.0.0.1:5173"}})  # Adjust with your frontend URL
+
+
+# Initialize the Gradio client
+client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/")
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['files']
+    
+    if uploaded_file:
+        # Read the content of the uploaded file
+        file_content = uploaded_file.read()
+        
+        # Predict using Gradio client
+        result = client.predict("fix the html code such that it follows WCAG convention" + "\n" + file_content.decode(),
+                                api_name="/chat")
+        
+        stripped_string = result.rstrip('</s>')
+        file_list=[]
+        lines=stripped_string.split("\n")
+        for line in lines:
+            if "<" not in line:
+                file_list.append("<!--"+line+"-->"+"\n")
+            else:
+                file_list.append(line+"\n")
+        file=open('temp_file.html',"w+")
+        file.writelines(file_list)
+        file.flush()
+        file.close()
+        return jsonify({"message":"Changes Completed, Click on Download to Download your file"})
+
+@app.route('/download', methods=['GET'])
+def download_file():
+    file_path = 'temp_file.html'
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
